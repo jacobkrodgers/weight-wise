f207f042e098a4fa811a73078bdaceb39d49f9b9
jacobkrodgers <jacob.rodgers2@smail.astate.edu>
2023-03-04 20:01:29 -0600

    outline endpoints

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,20 @@ app.use(express.json());
 
 const { PORT } = process.env;
 
-app.post('/api/users', registerUser); // Create an account
-app.post('/api/login', logIn); // Log in to an account
+app.post('/users', registerUser); // Create an account
+app.post('/login', logIn); // Log in to an account
+
+// post('/user/weight-tracking') ---- Jeffrey
+// get('/user/weight-tracking')  ---- Jeffrey
+
+// post('/user/progress-photos') ---- Jacob
+// get('/user/progress-photos')  ---- Jacob
+
+// post('/user/profile')         ---- Jacob
+// get('/user/profile')          ---- Jacob
+
+// post('/user/badges')          ---- Jacob
+// get('/user/badges')           ---- Jacob
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
