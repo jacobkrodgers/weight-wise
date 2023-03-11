89b488a713ee73eb8a27e5d6b9108059217c1286
jacobkrodgers <jacob.rodgers2@smail.astate.edu>
2023-03-11 12:33:34 -0600

    Clean up/fix relation

--- src/models/ProgressionPhotosModel.ts
+++ src/models/ProgressionPhotosModel.ts
@@ -1,17 +1,1 @@
-/* eslint-disable prettier/prettier */
-import { AppDataSource } from '../dataSource';
-import { ProgressionPhotos } from '../entities/ProgressionPhotos';
-
-const photoRepository = AppDataSource.getRepository(ProgressionPhotos);
-
-async function addPhoto(userId: string, photo: string): Promise<ProgressionPhotos | undefined> {
-    if (!photoRepository.findOne({ where: { userId } })) {
-        let newProgressionPhoto = new ProgressionPhotos();
-        const uploadDate = new Date();
-        newProgressionPhoto.begingingPhoto = { photo, uploadDate };
-        newProgressionPhoto = await photoRepository.save(newProgressionPhoto);
-        return newProgressionPhoto;
-    }
-}
-
-export { addPhoto };
+// Implement once we have covered updating relations
